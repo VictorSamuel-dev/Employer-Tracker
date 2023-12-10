680515ed0ef09233898cfa21f1af671f80ec39b1
VictorSamuel-dev <142364059+VictorSamuel-dev@users.noreply.github.com>
2023-12-10 12:04:18 -0500

    updated codes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "JumpingJack@23",
     database: "employeeTracker_db",
 });
 
